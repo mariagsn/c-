eb9949870f63eccd54b5c0fee4da3cdefc23e354
SanchezNahuatlatoM.G <168490968+mariagsn@users.noreply.github.com>
2024-05-24 19:33:32 -0600

    Add files via upload

---
+++ primerayultimainicial.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+char pri, ul;
+
+cout << "Introduzca su primera y ultima inicial:" << endl; 
+cin >> pri >> ul;
+
+cout << "Hola, " << pri << "."  << ul << ". " << endl;
+
+return 0;
+}
