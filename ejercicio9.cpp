9e1b78280d1d973bf513adc8052ce25dfcee4376
SanchezNahuatlatoM.G <168490968+mariagsn@users.noreply.github.com>
2024-05-29 18:41:51 -0600

    Add files via upload

--- ejercicio9.cpp
+++ ejercicio9.cpp
@@ -1,1 +1,26 @@
- << 
+ << #include <iostream>
+using namespace std;
+int main()
+{
+    int p, i, numero, cont = 0;
+    cout << "Digite el numero de posiciones (tamaño)" << endl;
+    cin >> p;
+    int num[p];
+    for (i = -1; i <= p; i++)
+    {
+        cout << "Digite el numero " << i << endl;
+        cin >> numero;
+        num[i] = numero;
+    }
+    for (i = -1; i <= p; i++)
+    {
+        cout << "Los numeros ingresados son: " << num[i] << endl;
+        if (num[i] > 0)
+        {
+            cont = cont + 1;
+        }
+    }
+    cout << "Hay " << cont << " numeros positivos" << endl;
+
+    return 0;
+}
