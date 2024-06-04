3e05732ee6bcfb9817cc7e7db3842a0c5b5d51bc
SanchezNahuatlatoM.G <168490968+mariagsn@users.noreply.github.com>
2024-06-03 21:50:46 -0600

    Add files via upload

---
+++ ejercicio30.cpp
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int main()
+{
+int i, num [100];
+
+  for (i=1; i<= 100; i++)
+  {
+  num [i]=i;
+  }
+  for (i=1;i<=100; i++) 
+  {
+  printf ("%i\n", num[i]);
+  }
+return 0;
+
+}
